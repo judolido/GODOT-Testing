b06625d72f2950b210c5ba35f3c275644cfa5e84
Viktors Matuls <viktorito10g@inbox.lv>
2025-01-09 23:39:17 +0200

    2Dgame is ready

--- main.gd
+++ main.gd
@@ -5,22 +5,26 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
-	
+#func _ready():
+#	pass
 	
 	
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
